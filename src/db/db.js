b2c642404dfb6e2fa3d7243a2b702438c615be2a
Vinayak Kamble <110101863+vinayakmk19@users.noreply.github.com>
2025-02-01 11:48:46 +0530

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
-require('dotenv').config();
-import {DB_NAME} from '../constants.js'
+import dotenv from "dotenv";
+import { DB_NAME } from "../constants.js";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     const db = mongoose.connection;
 
-    db.on('error', console.error.bind(console, 'Error Connecting to Database!'));
+    db.on("error", console.error.bind(console, "Error Connecting to Database!"));
 
-    db.once('open', function () {
+    db.once("open", function () {
       console.log("Successfully Connected to Database :: MongoDB");
     });
 
