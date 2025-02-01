dd3ddc1f8b41da77eabb6efc574a5c237066cfcb
Vinayak Kamble <110101863+vinayakmk19@users.noreply.github.com>
2025-02-01 11:57:06 +0530

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,24 +1,16 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-import { DB_NAME } from "../constants.js";
+import {DB_NAME} from '../constants.js'
 
-dotenv.config();
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(` MONGODB Connected !! DB HOST: ${connectionInstance.connection.host}`)
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    const db = mongoose.connection;
-
-    db.on("error", console.error.bind(console, "Error Connecting to Database!"));
-
-    db.once("open", function () {
-      console.log("Successfully Connected to Database :: MongoDB");
-    });
-
-  } catch (error) {
-    console.error("MONGO DB connection error", error);
-    process.exit(1);
-  }
-};
+    } catch (error) {
+        console.log("MONGO DB connection error", error);
+        process.exit(1);
+        
+    }
+}
 
 export default connectDB;
